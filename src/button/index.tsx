06e304d2c0eb0e40c6309c7e4c2104a30b7ec200
Romeo Van Snick <romeovs@gmail.com>
2022-08-01 17:03:23 +0200

    Add Button component

---
+++ src/button/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import classnames from "classnames"
+
+import css from "./styles.module.css"
+
+export type ButtonProps = {
+  type: "button" | "submit" | "reset"
+  primary?: boolean
+  children: React.ReactNode
+}
+
+export function Button(props: ButtonProps): React.ReactElement {
+  const { primary, type, children } = props
+  return (
+    <button type={type} className={classnames(css.button, primary && css.primary)}>
+      {children}
+    </button>
+  )
+}
